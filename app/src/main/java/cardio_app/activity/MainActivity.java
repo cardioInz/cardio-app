5d42cab0553cf45c69cabaa0871ca85685464d5b
PapillonBleu <ludwiczak.b@gmail.com>
2016-11-09 00:22:31 +0100

    Initial menu + views changes

--- app/src/main/java/cardio_app/activity/MainActivity.java
+++ app/src/main/java/cardio_app/activity/MainActivity.java
@@ -2,21 +2,17 @@ package cardio_app.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 import cardio_app.R;
+import cardio_app.activity.diary.AddDiaryActivity;
 import cardio_app.activity.diary.DiaryActivity;
 import cardio_app.activity.drug.DrugsActivity;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,24 +21,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     @Override
@@ -52,52 +30,19 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void showMeasuresList(View view) {
+        Intent intent = new Intent(this, DiaryActivity.class);
+        startActivity(intent);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+    public void addMeasure(View view) {
+        Intent intent = new Intent(this, AddDiaryActivity.class);
+        startActivity(intent);
+    }
 
-        if (id == R.id.menu_diary) {
-            Intent intent = new Intent(this, DiaryActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.menu_import_export) {
-
-        } else if (id == R.id.menu_genre_report) {
-
-        } else if (id == R.id.menu_advices) {
-
-        } else if (id == R.id.menu_reminders) {
-            Intent intent = new Intent(this, DrugsActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.menu_questionnaire) {
-
-        } else if (id == R.id.menu_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+    public void showDrugsList(View view) {
+        Intent intent = new Intent(this, DrugsActivity.class);
+        startActivity(intent);
     }
 
 }
