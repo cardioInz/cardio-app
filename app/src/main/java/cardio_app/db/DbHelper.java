03f920df65ca48b8366301b4c088d1ef9c94070b
Marek Tokarski <mautokar@gmail.com>
2016-11-22 23:35:42 +0100

    added import data functionallity

--- app/src/main/java/cardio_app/db/DbHelper.java
+++ app/src/main/java/cardio_app/db/DbHelper.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.stmt.DeleteBuilder;
 import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.stmt.Where;
 import com.j256.ormlite.support.ConnectionSource;
@@ -16,6 +17,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -177,5 +179,30 @@ public class DbHelper extends OrmLiteSqliteOpenHelper {
         result.put("pressures", pressures);
 
         return result;
+    }
+
+    public void importFromJson(JSONObject data) throws JSONException, SQLException, ParseException {
+        Dao<Drug, Integer> drugDao = getDao(Drug.class);
+        int drugsDeleted = drugDao.deleteBuilder().delete();
+        Log.d(TAG, "Delete " + drugsDeleted + " drugs");
+
+        Dao<PressureData, Integer> pressureDao = getDao(PressureData.class);
+        int pressureDeleted = pressureDao.deleteBuilder().delete();
+        Log.d(TAG, "Delete " + pressureDeleted + " pressures");
+
+        JSONArray drugs = data.getJSONArray("drugs");
+        JSONArray pressures = data.getJSONArray("pressures");
+
+        for (int i = 0; i < drugs.length(); i++) {
+            JSONObject drugObject = drugs.getJSONObject(i);
+            Drug drug = Drug.convert(drugObject);
+            drugDao.create(drug);
+        }
+
+        for (int i = 0; i < pressures.length(); i++) {
+            JSONObject pressureObject = pressures.getJSONObject(i);
+            PressureData pressureData = PressureData.convert(pressureObject);
+            pressureDao.create(pressureData);
+        }
     }
 }
